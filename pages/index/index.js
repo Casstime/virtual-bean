498a70bd2273663c3bdd139a50aa181501ea6037
Javen <914301050@qq.com>
2017-04-20 21:21:02 +0800

    height scroll

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,32 @@
 // pages/index/index.js
 Page({
-  data:{},
+  data:{
+    members: [
+      {id: '1', nickname: 'a'},
+      {id: '2', nickname: 'b'},
+      {id: '3', nickname: 'c'},
+      {id: '4', nickname: 'c'},
+      {id: '5', nickname: 'c'},
+      {id: '6', nickname: 'c'},
+      {id: '7', nickname: 'c'},
+      {id: '8', nickname: 'c'},
+      {id: '9', nickname: 'c'},
+      {id: '10', nickname: 'c'},
+      {id: '11', nickname: 'd'},
+      {id: '12', nickname: 'e'},
+      {id: '13', nickname: 'f'}
+    ],
+    plain: false
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+    var self = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        console.log(res);
+        self.windowHeihgt = res.windowHeight;
+      }
+    });
   },
   onReady:function(){
     // 页面渲染完成
@@ -15,5 +39,8 @@ Page({
   },
   onUnload:function(){
     // 页面关闭
+  },
+  onSendBean: function () {
+    console.log('send bean');
   }
 })
