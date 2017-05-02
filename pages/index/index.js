04302fb124ff2c3e4eac74110e754dd8e6b0fad5
Javen Leung <914301050@qq.com>
2017-05-02 19:25:52 +0800

    解决冲突

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,17 +4,10 @@ Page({
     userInfo: {},
     groupId: '',
     members: [],
-    plain: false,
     windowHeight: 0
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
-  },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
     var app = getApp();
     var self = this;
     var globalData = app.globalData;
@@ -57,6 +50,12 @@ Page({
         }
       });
     });
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
   },
   onHide:function(){
     // 页面隐藏
