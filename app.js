4369eb7e52ee8a604a33b889f9a204bda3098b25
Javen Leung <914301050@qq.com>
2017-05-03 23:21:31 +0800

    登录逻辑

--- app.js
+++ app.js
@@ -1,51 +1,58 @@
+const config = require('./config');
 
-App({
+const appConfig = {
   onLaunch: function () {
     //调用API从本地缓存中获取数据
   },
   getUserInfo: function (cb) {
-    var that = this;
+    const that = this;
     if (this.globalData.userInfo) {
-      typeof cb == "function" && cb(this.globalData.userInfo);
+      typeof cb === "function" && cb(this.globalData.userInfo);
     } else {
       //调用登录接口
       wx.login({
         success: function (response) {
           console.log('返回的code', response.code);
           if (response.code) {
-            wx.request({
-              url: 'https://www.javenleung.com/login',
-              data: {
-                js_code: response.code
-              },
-              method: 'POST',
-              success: function (result) {
-                console.log('登录成功返回', result.data);
-                wx.setStorage({
-                  key: "openid",
-                  data: result.data.openid,
-                  success: function () {
-                    wx.getUserInfo({
-                      withCredentials: true,
-                      success: function (res) {
-                        console.log('向微信请求用户信息返回', res);
-                        that.globalData.userInfo = Object.assign({}, {userId: result.data.userId, openid: result.data.openid}, res.userInfo);
-                        console.log('global', that.globalData.userInfo);
-                        typeof cb == "function" && cb(that.globalData.userInfo)
+            wx.getUserInfo({
+              withCredentials: true,
+              success: function (res) {
+                console.log('向微信请求用户信息返回', res);
+                that.globalData.userInfo = Object.assign({}, res.userInfo);
+                console.log('global', that.globalData.userInfo);
+
+                wx.request({
+                  url: `${config.origin}/login`,
+                  data: {
+                    code: response.code,
+                    encryptedData: res.encryptedData,
+                    iv: res.iv,
+                    rawData: res.rawData,
+                    signature: res.signature
+                  },
+                  method: 'POST',
+                  success: function (result) {
+                    console.log('登录成功返回', result.data);
+                    that.globalData.userInfo = Object.assign({}, that.globalData.userInfo,  {userId: result.data.userId, openid: result.data.openid});
+                    typeof cb === "function" && cb(that.globalData.userInfo);
+                    wx.setStorage({
+                      key: "openid",
+                      data: result.data.openid,
+                      success: function () {
+
                       }
                     });
+                  },
+                  fail: function() {
+                    // fail
+                  },
+                  complete: function() {
+                    // complete
                   }
                 });
-              },
-              fail: function() {
-                // fail
-              },
-              complete: function() {
-                // complete
               }
-            })
+            });
           }
-         
         }
       });
     }
@@ -53,4 +60,6 @@ App({
   globalData: {
     userInfo: null
   }
-})
+};
+
+App(appConfig);
